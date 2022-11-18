75170ac42319823cfd3ba4a54cb3c474d18396ac
김성현 <shkim061198@gmail.com>
2022-11-18 17:41:41 +0900

    feat: lec5 observer로 무한스크롤 만들기

--- vanilla_JS/infinityScroll/src/PhotoList.js
+++ vanilla_JS/infinityScroll/src/PhotoList.js
@@ -1,13 +1,34 @@
 export default function PhotoList({ $target, initialState, onScrollEnded }) {
-  let isInitialize = false;
   const $photoList = document.createElement("div");
+
   $target.appendChild($photoList);
   this.state = initialState;
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const { isLoading, totalCount, photos } = this.state;
+
+        if (entry.isIntersecting && !isLoading) {
+          console.log("entry 화면 끝", entry);
+          if (totalCount > photos.length) {
+            onScrollEnded();
+          }
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
 
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
   };
+
+  let isInitialize = false;
+  let $lastLi = null;
 
   this.render = () => {
     if (!isInitialize) {
@@ -26,23 +47,24 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
         const $li = document.createElement("li");
 
         $li.setAttribute("data-id", photo.id);
-        $li.style = "list-style:none";
+        $li.style = "list-style:none;min-height: 500px";
         $li.innerHTML = `<img style="width:100%" src="${photo.imagePath}"/>`;
 
         $photos.appendChild($li);
       }
     });
+
+    const $nextLi = $photos.querySelector("li:last-child");
+
+    if ($nextLi !== null) {
+      if ($lastLi !== null) {
+        observer.unobserve($lastLi);
+      }
+
+      $lastLi = $nextLi;
+      observer.observe($lastLi);
+    }
   };
 
   this.render();
-
-  window.addEventListener("scroll", () => {
-    const { isLoading, totalCount, photos } = this.state;
-
-    const isScrollEnded = window.innerHeight + window.scrollY + 100 >= document.body.offsetHeight;
-
-    if (isScrollEnded & !isLoading && photos.length < totalCount) {
-      onScrollEnded();
-    }
-  });
 }
