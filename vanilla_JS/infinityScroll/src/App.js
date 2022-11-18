aa91d45ef15bbc56e982a57f57d9bab31db23253
김성현 <shkim061198@gmail.com>
2022-11-18 16:29:36 +0900

    feat: ~lec3 무한스크롤 버튼으로 구현하기

--- vanilla_JS/infinityScroll/src/App.js
+++ vanilla_JS/infinityScroll/src/App.js
@@ -5,29 +5,53 @@ export default function App({ $target }) {
   const $h1 = document.createElement("h1");
 
   $h1.innerText = "Cat Photos";
+  $h1.style.textAlign = "center";
   $target.appendChild($h1);
 
   this.state = {
     limit: 5,
-    start: 0,
+    nextstart: 0,
     photos: [],
+    isLoading: false,
   };
 
   this.setState = (nextState) => {
-    this.state - nextState;
-    photoListComponent.setState(nextState.photos);
+    this.state = nextState;
+    photoListComponent.setState({
+      isLoading: this.state.isLoading,
+      photos: this.state.photos,
+    });
   };
 
   const photoListComponent = new PhotoList({
     $target,
-    initialState: this.state.photos,
+    initialState: {
+      isLoading: this.state.isLoading,
+      photos: this.state.photos,
+    },
+    onScrollEnded: async () => {
+      await fetchPhotos();
+    },
   });
 
   const fetchPhotos = async () => {
-    const photos = await request(`/cat-photos?_limit=5&_start=0`);
     this.setState({
       ...this.state,
-      photos,
+      isLoading: true,
+    });
+
+    const { limit, nextstart } = this.state;
+    const photos = await request(`/cat-photos?_limit=${limit}&_start=${nextstart}`);
+
+    this.setState({
+      ...this.state,
+      nextstart: nextstart + limit,
+      photos: [...this.state.photos, ...photos],
+    });
+
+    this.setState({
+      ...this.state,
+      isLoading: false,
     });
   };
 
--- vanilla_JS/infinityScroll/src/PhotoList.js
+++ vanilla_JS/infinityScroll/src/PhotoList.js
@@ -1,5 +1,6 @@
 export default function PhotoList({ $target, initialState, onScrollEnded }) {
-  const $photoList = document.createElement("ul");
+  let isInitialize = false;
+  const $photoList = document.createElement("div");
   $target.appendChild($photoList);
   this.state = initialState;
 
@@ -9,19 +10,45 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
   };
 
   this.render = () => {
-    //prettier-ignore
-    $photoList.innerHTML = `
-      <ul>
-        ${this.state.map((photo) => {
-          return `
-            <li>
-              <img width="100%" src="${photo.imagePath}"/>
-            </li>
-          `;
-        }).join("")}
-      </ul>
-      <button class="PhotoList__loadMore">Load More</button>
-    `;
+    if (!isInitialize) {
+      $photoList.innerHTML = `
+        <ul class="PhotoList__photos" style="padding-left:0"></ul>
+        <button class="PhotoList__loadMore" style="width:100%;height:200px">Load More</button>
+      `;
+
+      isInitialize = true;
+    }
+
+    const { photos } = this.state;
+    const $photos = $photoList.querySelector(".PhotoList__photos");
+
+    photos.forEach((photo) => {
+      if ($photos.querySelector(`[data-id="${photo.id}"]`) === null) {
+        const $li = document.createElement("li");
+
+        $li.setAttribute("data-id", photo.id);
+        $li.style = "list-style:none";
+        $li.innerHTML = `<img style="width:100%" src="${photo.imagePath}"/>`;
+
+        $photos.appendChild($li);
+      }
+    });
   };
+
   this.render();
+
+  $photoList.addEventListener("click", (e) => {
+    console.log(this.state.isLoading);
+    if (e.target.className === "PhotoList__loadMore" && !this.state.isLoading) {
+      onScrollEnded();
+    }
+  });
+
+  window.addEventListener("scroll", () => {
+    const isScrollEnded = window.innerHeight + window.scrollY + 100 >= document.body.offsetHeight;
+
+    if (isScrollEnded) {
+      onScrollEnded();
+    }
+  });
 }
