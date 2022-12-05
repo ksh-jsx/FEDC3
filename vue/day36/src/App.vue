75d75911ee42c77fc3939f54f4682ef476ee208b
김성현 <shkim061198@gmail.com>
2022-12-05 22:28:14 +0900

    feat: day36 예제1

---
+++ vue/day36/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="reverseMsg">Click!</button>
+    <Hello :message="msg" name="hello" />
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: "Hello Vue!",
+    };
+  },
+  methods: {
+    reverseMsg() {
+      this.msg = this.msg.split("").reverse().join("");
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background: red;
+  color: #fff;
+}
+</style>
---
+++ vue/day36/src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 @click="updateMessage">{{ newMessage }}</h1>
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+    name: [String, Number],
+  },
+  data() {
+    return {
+      newMessage: this.message,
+    };
+  },
+  methods: {
+    updateMessage() {
+      this.newMessage = "Goods!";
+    },
+  },
+};
+</script>
