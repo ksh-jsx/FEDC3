f85cab12b5d1758bab8404475300a9f5b3b9de4a
김성현 <shkim061198@gmail.com>
2022-12-16 18:57:40 +0900

    story 예제 추가

---
+++ react/login_ex/src/components/Input.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const Input = styled.input`
+  padding: 4px 6px;
+  height: 28px;
+  font-size: 14px;
+  background-color: white;
+  width: 100%;
+  display: block;
+  box-sizing: border-box;
+  border-radius: 4px;
+`;
+
+export default Input;
